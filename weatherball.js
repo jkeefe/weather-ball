f485c6ca82101dddba6fd71add589435803e29a5
John Keefe <john@reallygoodsmarts.nyc>
2025-11-24 22:04:36 -0500

    switched to import

--- weatherball.js
+++ weatherball.js
@@ -1,4 +1,4 @@
-const { SerialPort } = require('serialport')
+import { SerialPort } from 'serialport'
 const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 115200 })
 
 port.write('{"ps": 2}', function (err) {
