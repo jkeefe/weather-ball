cee663f5a6740fbf6bd75829b40ad86e773540c5
John Keefe <john@reallygoodsmarts.nyc>
2025-12-30 18:11:57 -0500

    enabled weather ball

--- weather-ball.js
+++ weather-ball.js
@@ -15,8 +15,8 @@ const fetch = fetchRetry(global.fetch, {
     // retryDelay: 2000 // 2 seconds
 });
 
-// // set the serial port
-// const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 115200 })
+// set the serial port
+const port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 115200 })
 
 // constants
 // const FORECAST_URL = `https://api.weather.gov/gridpoints/OKX/33,37/forecast/hourly`
@@ -190,7 +190,7 @@ const main = async () => {
     if (!preset) { preset = 26 }
     console.log(`Ball Preset: ${preset}`)
 
-    // await setWeatherBall(preset)
+    await setWeatherBall(preset)
 
     console.log("Done")
 
